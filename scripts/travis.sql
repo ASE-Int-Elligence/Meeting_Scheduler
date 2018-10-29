a29374af6d9af0cbb99f4f980ba001d627ff8944
oliwang <Dg1AvHjj>
2018-10-29 13:45:33 -0400

    use script

---
+++ scripts/travis.sql
@@ -0,0 +1,10 @@
+create database IF NOT EXISTS 'ase_project_db';
+
+CREATE TABLE IF NOT EXISTS `user_credentials` (
+    `username` varchar(50) DEFAULT NULL,
+    `nameFirst` varchar(50) DEFAULT NULL,
+    `nameLast` varchar(50) DEFAULT NULL,
+    `password` varchar(50) DEFAULT NULL,
+    `email` varchar(150) DEFAULT NULL,
+    `DOB` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
