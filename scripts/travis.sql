ad3cb6f8566c54ca59116d958ba08e976d8e901f
oliwang <Dg1AvHjj>
2018-10-29 14:55:38 -0400

    update sql

--- scripts/travis.sql
+++ scripts/travis.sql
@@ -7,4 +7,4 @@ CREATE TABLE IF NOT EXISTS `user_credentials` (
     `password` varchar(50) DEFAULT NULL,
     `email` varchar(150) DEFAULT NULL,
     `DOB` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
