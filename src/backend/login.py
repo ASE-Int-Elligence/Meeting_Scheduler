cd6d538f139606869dc7bee0e60306ba5f04de00
oliwang <Dg1AvHjj>
2018-10-30 17:01:58 -0400

    add testcase for login

--- src/backend/login.py
+++ src/backend/login.py
@@ -1,27 +1,60 @@
 
-from backend.Database_op import *
-# import Database_op
-import copy
+# from backend.Database_op import *
+# # import Database_op
+# import copy
 
-def login_page(username,password):
+# def login_page(username,password):
 
-    result = find_by_primary_key("user_credentials", username)
-    if password == result[0]['password']:
-        return True
-    else:
+#     result = find_by_primary_key("user_credentials", username)
+#     if password == result[0]['password']:
+#         return True
+#     else:
+#         return False
+
+# def sign_up(username,password, nameLast,nameFirst,email,DOB):
+#     template={}
+#     template['username']=username
+#     template['nameFirst'] = nameFirst
+#     template['nameLast'] = nameLast
+#     template['email'] = email
+#     template['DOB'] = DOB
+#     template['password'] = password
+#     result = create_user("user_credentials",template)
+#     return result
+
+# if __name__ == '__main__':
+#     result = sign_up("user4","password3","priya","chandana","email@gmail.com","125")
+#     print(result)
+
+from Models import db, User
+
+def login_username(username, password):
+    user = User.query.filter_by(username=username).first()
+    if not user:
         return False
+    else:
+        if password == user.password:
+            return True
+        else:
+            return False
 
-def sign_up(username,password, nameLast,nameFirst,email,DOB):
-    template={}
-    template['username']=username
-    template['nameFirst'] = nameFirst
-    template['nameLast'] = nameLast
-    template['email'] = email
-    template['DOB'] = DOB
-    template['password'] = password
-    result = create_user("user_credentials",template)
-    return result
+def login_email(email, password):
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return False
+    else:
+        if password == user.password:
+            return True
+        else:
+            return False
 
-if __name__ == '__main__':
-    result = sign_up("user4","password3","priya","chandana","email@gmail.com","125")
-    print(result)
+def create_user(email, password, username, firstname, lastname):
+    user = User(email=email, password=password, username=username, firstname=firstname, lastname=lastname)
+    db.session.add(user)
+    db.session.commit()
+
+if __name__ == "__main__":
+    print(login_username("test", "alsotest"))
+    print(login_username("admin", "admin"))
+
+
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,16 +1,42 @@
-from backend.login import *
+from backend.login import login_email, login_username, create_user
+from backend.Models import db, User
 # import main
 import unittest
 
-class login_and_signup_unittest(unittest.TestCase):
+# class login_and_signup_unittest(unittest.TestCase):
 
-    def test_login_success(self):
-        res = login_page("user1", "password")
-        self.assertTrue(res)
+#     def test_login_success(self):
+#         res = login_page("user1", "password")
+#         self.assertTrue(res)
 
-    def signup_success(self):
-        res = sign_up("user7","password7","G","chandana","email@gmail.com","125")
-        self.assertEqual(res,"The row has been inserted")
+#     def signup_success(self):
+#         res = sign_up("user7","password7","G","chandana","email@gmail.com","125")
+#         self.assertEqual(res,"The row has been inserted")
+
+class LoginTest(unittest.TestCase):
+
+    def test_login_email(self):
+        self.assertTrue(login_email("example@gmail.com", "admin"))
+        self.assertFalse(login_email("example@gmail.com", "a"))
+        self.assertFalse(login_email("example@hotmail.com", "admin"))
+        self.assertFalse(login_email("example", "a"))
+
+    def test_login_username(self):
+        self.assertTrue(login_username("admin", "admin"))
+        self.assertFalse(login_username("admin", "a"))
+        self.assertFalse(login_username("e1234", "admin"))
+        self.assertFalse(login_username("example", "a"))
+
+    def test_create_user(self):
+        create_user(email="testuser@gmail.com", password="test", username="testuser", firstname="Eddie", lastname="Black")
+
+        user = User.query.filter(email="testuser@gmail.com")
+
+        self.assertEqual(user.password, 'test')
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.firstname, 'Eddie')
+        self.assertEqual(user.lastname, 'Black')
+        
 
 if __name__ == '__main__':
     unittest.main()
