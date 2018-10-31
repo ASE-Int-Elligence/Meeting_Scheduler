b06d7d34662c69b2a1aa711191279002d49d74d1
oliwang <Dg1AvHjj>
2018-10-30 20:09:52 -0400

    post-commit

--- scripts/post_commit.sh
+++ scripts/post_commit.sh
@@ -4,9 +4,11 @@
 cd src
 today=`date '+%Y_%m_%d__%H_%M_%S'`
 filename1="/home/el/myfile/$today-bug.log"
+touch $filename1
 prospector --strictness veryhigh > $filename1
 
 #pytest
 cd ../tests
+touch $filename2
 filename2="/home/el/myfile/$today-pytest.log"
 pytest *.py > $filename2
