1da84f7b31f815bdd834457aeebed352de98a16c
oliwang <Dg1AvHjj>
2018-10-30 20:13:19 -0400

    log

--- scripts/post_commit.sh
+++ scripts/post_commit.sh
@@ -3,12 +3,12 @@
 #style checker, bug finder
 cd src
 today=`date '+%Y_%m_%d__%H_%M_%S'`
-filename1="/home/el/myfile/$today-bug.log"
+filename1="$TRAVIS_BUILD_DIR/report/$today-bug.log"
 touch $filename1
 prospector --strictness veryhigh > $filename1
 
 #pytest
 cd ../tests
 touch $filename2
-filename2="/home/el/myfile/$today-pytest.log"
+filename2="$TRAVIS_BUILD_DIR/report/$today-pytest.log"
 pytest *.py > $filename2
