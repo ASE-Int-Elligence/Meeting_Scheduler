61af1ce80a957faeb6b84caf631dc551b66d2402
oliwang <Dg1AvHjj>
2018-11-07 14:35:32 -0500

    travis push to repo

--- scripts/post_commit.sh
+++ scripts/post_commit.sh
@@ -3,12 +3,12 @@
 #style checker, bug finder
 cd src
 today=`date '+%Y_%m_%d__%H_%M_%S'`
-filename1="$TRAVIS_BUILD_DIR/report/$today-bug.log"
+filename1="$TRAVIS_BUILD_DIR/report/post_commit/$today-bug.log"
 touch $filename1
 prospector --strictness veryhigh > $filename1
 
 #pytest
 cd ../tests
-filename2="$TRAVIS_BUILD_DIR/report/$today-pytest.log"
+filename2="$TRAVIS_BUILD_DIR/report/post_commit/$today-pytest.log"
 touch $filename2
 pytest *.py > $filename2
---
+++ scripts/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "oliwang"
+  git config --global user.name "jwang0614@gmail.com "
+}
+
+commit_log_files() {
+  git checkout -b gh-pages
+  git add . *.log
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-logs https://${GH_TOKEN}@github.com/ASE-Int-Elligence/Meeting_Scheduler.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages 
+}
+
+setup_git
+commit_log_files
+upload_files
