18913d3081e63b0afb59f9e24aec87f6dcaf511e
oliwang <Dg1AvHjj>
2018-11-07 15:42:46 -0500

    change push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,15 +6,17 @@ setup_git() {
 }
 
 commit_log_files() {
-  git checkout -b master
-  git add . *.log
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+    git pull
+#   git checkout -b master
+    git add . *.log
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
     git remote add origin-logs https://${GH_TOKEN}@github.com/ASE-Int-Elligence/Meeting_Scheduler.git
 #   git remote add origin-logs https://${GH_TOKEN}@github.com/ASE-Int-Elligence/Meeting_Scheduler.git > /dev/null 2>&1
-    git push --quiet --set-upstream origin-logs master
+    # git push --quiet --set-upstream origin-logs master
+    git push
 }
 
 setup_git
