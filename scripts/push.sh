a9b1ea2cdbe2a068d8c7af682e479115fbcb3184
oliwang <Dg1AvHjj>
2018-11-07 14:56:19 -0500

    bash.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -12,9 +12,9 @@ commit_log_files() {
 }
 
 upload_files() {
-    git remote add origin-logs https://${GH_TOKEN}@github.com/ASE-Int-Elligence/Meeting_Scheduler.git
+    git remote add origin https://${GH_TOKEN}@github.com/ASE-Int-Elligence/Meeting_Scheduler.git
 #   git remote add origin-logs https://${GH_TOKEN}@github.com/ASE-Int-Elligence/Meeting_Scheduler.git > /dev/null 2>&1
-    git push --quiet --set-upstream origin-logs
+    git push --quiet --set-upstream origin
 }
 
 setup_git
