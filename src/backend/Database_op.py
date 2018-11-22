5892fcb117b7282c10694d3449cf76513ecda380
Sai Srujan Chinta <srujanchinta8@gmail.com>
2018-11-21 19:18:28 -0500

    Backend modification

--- src/backend/Database_op.py
+++ src/backend/Database_op.py
@@ -1,9 +1,14 @@
 import pymysql
 import json
 
-# cnx = pymysql.connect(host='localhost', port=3306, user='dbuser', password='dbuser', db='ase_project_db', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-cnx = pymysql.connect(host='localhost', port=3306, user='travis', password='', db='ase_project_db', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+cnx = pymysql.connect(host='localhost',
+                              user='int_elligence',
+                              password='int_elligence',
+                              db='ase_project',
+                              charset='utf8mb4',
+                              cursorclass=pymysql.cursors.DictCursor)
 
+group_id = 0
 
 def run_q(q, args, fetch=False):
     cursor = cnx.cursor()
@@ -15,46 +20,88 @@ def run_q(q, args, fetch=False):
     cnx.commit()
     return result
 
-def template_to_where_clause(t):
+def templateToWhereClause_old(t):
     s = ""
-    for (k, v) in t.items():
+    for (k,v) in t.items():
         if s != "":
             s += " AND "
-        s = s + k
-        s = s + " ='"
-        if type(v) == list:
-            s = s + v[0]
-        else:
-            s = s + v
-        s = s + "'"
+        s += k + "='" + v + "'"
 
     if s != "":
-        s = "WHERE " + s
-
+        s = "WHERE " + s;
+    #print (s)
     return s
 
-
-def find_by_primary_key(table, username):
-
-    q = "select password from " + table + " where username = '"+username+"'"
-    #q = "select * from " + table
+def find_by_pk(table, pk, fields = None):
+    #print (pk)
+    q = "SELECT `COLUMN_NAME` FROM `information_schema`.`COLUMNS` WHERE (`TABLE_SCHEMA` = 'ase_project') AND (`TABLE_NAME` = '" + table + "' ) AND (`COLUMN_KEY` = 'PRI');"
+    primarykeys = run_q(q, None, True)
+    print (primarykeys)
+    template = {}
+    template[primarykeys[0]['COLUMN_NAME']] = pk
+    wc = templateToWhereClause_old(template)
+    if fields == None:
+        q = "select * from "+ table + " " + wc
+    else:
+        q = "select " + fields + " from " + table + " " + wc
+    print (q)
     result = run_q(q, None, True)
-    #result = q
     return result
 
-def create_user(table,template):
-    try:
-        columns = ""
-        values = ""
-        for columnName in template.keys():
-            columns = columns + columnName + ", "
-        columns = columns[:-2]
-        for columnValue in template.values():
-            values = values + "'" + columnValue + "'" + ", "
-        values = values[:-2]
-        sql = "INSERT INTO " + table + "(" + columns + ")" + "values" + "(" + values + ")"
-        run_q(sql, None, True)
-        result = "The row has been inserted"
-    except Exception as e:
-        result = str(e)
+def insert(table, row):
+    col_name = ""
+    val_name = ""
+    for name,val in row.items():
+        col_name = col_name + name + ","
+        val_name = val_name + "'" + val + "'" + ","
+    col_name = col_name[:-1]
+    val_name = val_name[:-1]
+    query = "INSERT INTO " + table + " (" + col_name + ") VALUES (" + val_name + ");"
+    result = run_q(query, None, True)
+    res = "Insert Successful"
+    return res
+
+def find_partial(table, row):
+    #print (pk)
+    row["username"] = "'" + row["username"] + "%'"
+    q = "select username from user_credentials where username like " + row["username"]
+    print (q)
+    result = run_q(q, None, True)
+    return result
+
+def insert_group(table, row):
+    global group_id
+    group_id += 1
+    row["groupID"] = str(group_id)
+    col_name = ""
+    val_name = ""
+    for name,val in row.items():
+        col_name = col_name + name + ","
+        val_name = val_name + "'" + val + "'" + ","
+    col_name = col_name[:-1]
+    val_name = val_name[:-1]
+    query = "INSERT INTO " + table + " (" + col_name + ") VALUES (" + val_name + ")"
+    result = run_q(query, None, True)
+    res = "Insert Successful"
+    return res
+
+def find_groups(table, row):
+    #print (pk)
+    q = "select groupName from groups where username = '" + row["username"] + "'"
+    print (q)
+    result = run_q(q, None, True)
+    return result
+
+def print_indmeeting(table, row):
+    #print (pk)
+    q = "select meeting.meetingID, meeting.username, meeting.meetingLoc from meeting inner join groups on groups.meetingID = meeting.meetingID where username = '" + row["username"] + "' and groupName = '" + row["groupName"] + "'"
+    print (q)
+    result = run_q(q, None, True)
+    return result
+
+def print_indgroups(table, row):
+    #print (pk)
+    q = "select groups.username, user_credentials.nameFirst, user_credentials.nameLast from groups inner join user_credentials on groups.username = user_credentials.username where groupName = '" + row["groupName"] + "'"
+    print (q)
+    result = run_q(q, None, True)
     return result
---
+++ src/backend/ase_project.py
@@ -0,0 +1,91 @@
+import json
+
+# Include Flask packages
+from flask import Flask
+from flask import request
+import copy
+import ase_BO
+import requests
+# The main program that executes. This call creates an instance of a
+# class and the constructor starts the runtime.
+app = Flask(__name__)
+
+def parse_and_print_args():
+    fields = None
+    in_args = None
+    if request.args is not None:
+        in_args = dict(copy.copy(request.args))
+        fields = copy.copy(in_args.get('fields', None))
+    if fields:
+        del(in_args['fields'])
+    try:
+        print (request)
+        if request.data:
+            body = json.loads(request.data)
+        else:
+            body = None
+    except Exception as e:
+        print ("Got exception = ", e)
+        body = None
+    print("Request.args : ",json.dumps(in_args))
+    return in_args, fields, body
+
+
+@app.route('/login', methods = ['POST'])
+def login_page():
+    in_args, fields, body = parse_and_print_args()
+    print(body)
+    res= ase_BO.find_by_pk("user_credentials",body["username"])
+    if res[0]['password'] == body["password"]:
+        return json.dumps("Login Successful"), 200, {"content-type": "application/json; charset: utf-8"}
+    else:
+        return json.dumps("Login Failed"), 400, {"content-type": "application/json; charset: utf-8"}
+
+@app.route('/signup', methods = ['POST'])
+def signup_page():
+    in_args, fields, body = parse_and_print_args()
+    res= ase_BO.insert("user_credentials",body)
+    return "Signup successful"
+
+@app.route('/create_group', methods = ['POST'])   ## Get default meeting-id in body
+def create_group():
+    in_args, fields, body = parse_and_print_args()
+    res= ase_BO.insert_group("groups",body)
+    return "Group Creation successful"
+
+@app.route('/search_user', methods = ['POST'])
+def search_user():
+    in_args, fields, body = parse_and_print_args()
+    res= ase_BO.find_partial("user_credentials",body)
+    return json.dumps(res), 200, {"content-type": "application/json; charset: utf-8"}
+
+@app.route('/display_members', methods = ['POST'])      #username
+def display_members():
+    in_args, fields, body = parse_and_print_args()
+    res= ase_BO.find_partial("user_credentials",body)
+    return json.dumps(res), 200, {"content-type": "application/json; charset: utf-8"}
+
+@app.route('/display_groups', methods = ['POST'])      #print all groups that user belongs to
+def display_groups():
+    in_args, fields, body = parse_and_print_args()
+    res= ase_BO.find_groups("user_credentials",body)
+    ans = {}
+    i = 0
+    for r in res:
+        ans[str(i)] = r
+        i = i + 1
+    return json.dumps(ans), 200, {"content-type": "application/json; charset: utf-8"}
+
+@app.route('/individual_groups', methods = ['POST'])      #print all user_details where users belong to that group
+def ind_groups():
+    in_args, fields, body = parse_and_print_args()
+    res= ase_BO.print_indgroups("user_credentials",body)
+    ans = {}
+    i = 0
+    for r in res:
+        ans[str(i)] = r
+        i = i + 1
+    return json.dumps(ans), 200, {"content-type": "application/json; charset: utf-8"}
+
+if __name__ == '__main__':
+    app.run()
