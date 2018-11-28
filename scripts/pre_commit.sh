8f1645c825fbb64e971144795494dbd77738be44
Chandana_Priya <chandanapriya12@gmail.com>
2018-11-27 20:48:51 -0500

    reset post and pre commit

--- scripts/pre_commit.sh
+++ scripts/pre_commit.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-#today=`date '+%Y_%m_%d__%H_%M_%S'`
+today=`date '+%Y_%m_%d__%H_%M_%S'`
 # # build
 pip install -r ../requirements.txt
 python ../setup.py install
 
 # style checker, bug finder
 cd ../src
-filename1="../report/pre_commit/bug.log"
+filename1="../report/pre_commit/$today-bug.log"
 touch $filename1
 prospector --strictness veryhigh > $filename1
 
 # testing
 cd ../tests
 
-filename2="../report/pre_commit/pytest.log"
+filename2="../report/pre_commit/$today-pytest.log"
 touch $filename2
 pytest *.py > $filename2
-
-
-
-
-
-
