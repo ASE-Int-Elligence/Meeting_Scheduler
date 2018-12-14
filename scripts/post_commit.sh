02c92d52c63a8333d789b2b9555b523b22421165
Chandana_Priya <chandanapriya12@gmail.com>
2018-12-14 12:19:27 -0500

    travis

--- scripts/post_commit.sh
+++ scripts/post_commit.sh
@@ -7,7 +7,6 @@ filename1="$TRAVIS_BUILD_DIR/report/post_commit/$today-bug.log"
 touch $filename1
 prospector --strictness veryhigh > $filename1
 
-
 #pytest
 cd ../tests
 filename2="$TRAVIS_BUILD_DIR/report/post_commit/$today-pytest.log"
